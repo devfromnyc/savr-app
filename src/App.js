791f0f4a2884d56148de2287059c1d535f657cd6
devfromnyc <devfromnyc@gmail.com>
2023-08-15 16:15:20 -0400

    update: Added components to App Js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 
-function App() {
+import MainHeader from './shared/components/Navigation/MainHeader';
+import Hero from './shared/components/UserInterface/Hero';
+import HomepageFormContainer from './shared/components/Forms/HomepageFormContainer';
+import UserCardList from './shared/components/UserInterface/UserCardList';
+import PieChart from './shared/components/Graphs/PieChart';
+import AddModal from './shared/components/Forms/AddModal';
+import EditModal from './shared/components/Forms/EditModal';
+import Footer from './shared/components/Navigation/Footer';
+
+import { AuthContext } from '../src/shared/context/auth-context';
+
+const App = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [userId, setUserId] = useState(false);
+
+  const login = useCallback((uid) => {
+    setIsLoggedIn(true);
+    setUserId(uid);
+  }, []);
+
+  let displayContent;
+
+  if (isLoggedIn) {
+    displayContent =(
+    <React.Fragment>
+      <MainHeader showTrigger="true" />
+      <main className="width100">
+        <Hero
+        mainHeader="Welcome back!"
+        subHeader="You're doing great! Keep saving!"
+        />
+        <PieChart/>
+        <UserCardList />
+      </main>
+      <Footer />
+    </React.Fragment>
+    );
+  } else{
+    displayContent = (
+      <React.Fragment>
+        <MainHeader showTrigger="false" />
+        <main className="width100">
+          <Hero
+          mainHeader="Welcome to SAVR!"
+          subHeader="Please Login or Signup to continue"
+          />
+          <HomepageFormContainer />
+        </main>
+        <Footer />
+      </React.Fragment>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <AuthContext.Provider
+        value={{ isLoggedIn: isLoggedIn, userId: userId, login: login}}
+      >
+        <Router>
+          {displayContent}
+          <EditModal />
+          <AddModal />
+        </Router>
+      </AuthContext.Provider>
   );
 }
 
